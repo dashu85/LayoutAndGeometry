6d88d82a163d8ac768893302a079c86f50e3fa9e
Marcus Benoit <benoit.marcus@gmail.com>
2024-08-28 13:46:01 +0200

    Initial Commit

---
+++ LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Marcus Benoit on 28.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Marcus Benoit on 28.08.24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
